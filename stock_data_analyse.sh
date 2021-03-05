528f0fa36b528db6fa897c2108546f41cfc5c08b
callmefeifei <callmefeifei@163.com>
2021-03-05 15:05:59 +0800

    update:增加市场数据分析. --market_data_analyse

--- stock_data_analyse.sh
+++ stock_data_analyse.sh
@@ -2,6 +2,7 @@ cd /opt/app/stock_realtime_anaysis/
 d_1=`python -c 'import time;print time.strftime("%Y-%m-%d", time.localtime())'`
 d_2=`python -c 'import time;print time.strftime("%Y%m%d", time.localtime())'`
 d_3=`python -c 'import time;print time.strftime("%H_%M_%S", time.localtime())'`
-cd /opt/app/stock_realtime_anaysis/ && python filter_good_stock_pro.py --format_result result/$d_1/$d_2\_money_flow.txt --format_all > result/$d_1/$d_2\_zongjie.txt
 
-python filter_good_stock_pro.py --market_data_analyse > result/$d_1/stock_data_analyse\_$d_3.txt
+mkdir result/$d_1/market_anaylse &>> /dev/null  2>&1 &
+
+python filter_good_stock_pro.py --market_data_analyse > result/$d_1/market_anaylse/stock_data_analyse\_$d_3.txt
